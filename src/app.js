2a4711064b26999e61f195532f00be4207dc40c7
artem-fuhrmann <artem.fuhrmann@gmail.com>
2022-10-13 14:47:51 +0200

    Add about test. Change help title.

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help MEEEE!!!!',
+        title: 'Need help?',
         name: "Tom Fuhrmann"
     })
 })
